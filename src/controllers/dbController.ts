badc7a44b649efa51d3be96964072b3510e257ba
yavidor <yonatan@avidor.org>
2023-08-25 16:34:25 +0300

    OCP

--- src/controllers/dbController.ts
+++ src/controllers/dbController.ts
@@ -3,9 +3,9 @@ import { createEntity, readEntity, deleteEntity } from "../models/dbModel";
 
 const dbRouter = Router();
 
-dbRouter.get(["/read", "/read/id/:id"], async (req: Request, res: Response) => res.send(await readEntity(req.params?.id).then(console.log).catch(console.error)));
+dbRouter.get(["/read", "/read/id/:id"], async (req: Request, res: Response) => res.send(await readEntity(req.params?.id)));
 
-dbRouter.post("/add", async (req: Request, res: Response) => res.send(await createEntity(req.body).then(console.log).catch(console.error)));
+dbRouter.post("/add", async (req: Request, res: Response) => res.send(await createEntity(req.body)));
 
-dbRouter.delete("/delete", async (req: Request, res: Response) => res.send(await deleteEntity().then(console.log).catch(console.error)));
+dbRouter.delete("/delete", async (req: Request, res: Response) => res.send(await deleteEntity()));
 export default dbRouter;
