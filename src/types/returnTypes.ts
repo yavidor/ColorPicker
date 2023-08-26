b11988eaa59d42f60a228b6498c50394c5c61701
yavidor <yonatan@avidor.org>
2023-08-26 22:53:19 +0300

    Adding a table

--- src/types/returnTypes.ts
+++ src/types/returnTypes.ts
@@ -2,5 +2,5 @@ import { Document as mongoDocument, ObjectId } from "mongodb";
 
 type Entity = mongoDocument |
 { _id: ObjectId; first_name: string; last_name: string; color: string };
-type Get = { "message": mongoDocument | String; };
+type Get = { "message": Array<Entity> | String; };
 export { Get, Entity };
--- src/views/view.ts
+++ src/views/view.ts
@@ -1,3 +1,5 @@
+import { Entity, Get } from "../types/returnTypes";
+
 window.onload = () => {
   const picker = document.getElementById("colorpicker")! as HTMLInputElement;
   const firstName = document.getElementById("firstName")! as HTMLInputElement;
@@ -11,7 +13,17 @@ window.onload = () => {
     console.log((event.target as HTMLInputElement).value)
   ));
 
-  buttonGet.addEventListener("click", () => fetch("http://localhost:8080/db/read").then((res: Response) => res.json()).then(console.log));
+  const drawTable = ({ message }: Get) => {
+    console.log(message);
+    console.log(typeof message);
+    const tableContainer = document.getElementById("table-container")!;
+    const table = document.createElement("table")!;
+    const headersRow = document.createElement("tr")!;
+    (message as Array<Entity>).forEach((entity) => {
+      console.log(tableContainer, table, headersRow, entity);
+    });
+  };
+  buttonGet.addEventListener("click", () => fetch("http://localhost:8080/db/read").then((res: Response) => res.json()).then(drawTable));
   // buttonDelete.addEventListener("click", () => fetch("http://localhost:8080/db/delete", {
   //   method: "DELETE",
   // }).then((res: Response) => res.json()).then(console.log));
